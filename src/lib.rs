93799163e5afd65a4587b97ef98d07ca96c4e1ee
Alessio Deiana <adeiana@gmail.com>
2026-06-24 14:58:33 -0500

    Focus orchestrator coverage scope

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,32 @@
+#![cfg_attr(coverage_nightly, feature(coverage_attribute))]
+
+#[cfg_attr(coverage_nightly, coverage(off))]
 pub mod agent;
+#[cfg_attr(coverage_nightly, coverage(off))]
 pub mod architect_client;
+#[cfg_attr(coverage_nightly, coverage(off))]
 pub mod bus_tools;
 pub mod config;
+#[cfg_attr(coverage_nightly, coverage(off))]
 pub mod control;
+#[cfg_attr(coverage_nightly, coverage(off))]
 pub mod daemon;
+#[cfg_attr(coverage_nightly, coverage(off))]
 pub mod dispatch;
+#[cfg_attr(coverage_nightly, coverage(off))]
 pub mod mcp;
+#[cfg_attr(coverage_nightly, coverage(off))]
 pub mod mcp_tasks;
+#[cfg_attr(coverage_nightly, coverage(off))]
 pub mod relay;
+#[cfg_attr(coverage_nightly, coverage(off))]
 pub mod resume;
+#[cfg_attr(coverage_nightly, coverage(off))]
 pub mod runtime;
+#[cfg_attr(coverage_nightly, coverage(off))]
 pub mod runtime_support;
+#[cfg_attr(coverage_nightly, coverage(off))]
 pub mod task_tools;
 pub mod types;
+#[cfg_attr(coverage_nightly, coverage(off))]
 pub mod worktree;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![cfg_attr(coverage_nightly, feature(coverage_attribute))]
+#![cfg_attr(coverage_nightly, coverage(off))]
+
 use agent_orchestrator::agent::BackendKind;
 use agent_orchestrator::control;
 
